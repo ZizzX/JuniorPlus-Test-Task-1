7d0a0f4125c0d430a8bcd82e467cd329be7893aa
Aziz I <zizzxiii@gmail.com>
2025-09-16 23:39:18 +0500

    feat: добавить обработку исключений в функцию bootstrap для улучшения устойчивости приложения

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { App } from './app';
+import { ExceptionFilter } from './errors/exception.filter';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/user.controller';
 
 async function bootstrap() {
 	const logger = new LoggerService();
 	const userController = new UserController(logger);
-	const app = new App(logger, userController);
+	const exceptionFilter = new ExceptionFilter(logger);
+
+	const app = new App(logger, userController, exceptionFilter);
 	await app.init();
 }
 
