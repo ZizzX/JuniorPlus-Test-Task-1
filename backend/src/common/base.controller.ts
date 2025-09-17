f6de34cd07037913942028046d5db0f73836ab57
Aziz I <zizzxiii@gmail.com>
2025-09-17 19:42:37 +0500

    feat: добавить методы для отправки различных HTTP-ответов в базовом контроллере

--- backend/src/common/base.controller.ts
+++ backend/src/common/base.controller.ts
@@ -25,4 +25,45 @@ export abstract class BaseController {
 	public getRouter(): Router {
 		return this.router;
 	}
+
+	public send<T>(res: any, code: number, data: T): void {
+		res.type('application/json');
+		res.status(code).json(data);
+	}
+
+	public ok<T>(res: any, data: T): void {
+		this.send<T>(res, 200, data);
+	}
+
+	public created<T>(res: any, data: T): void {
+		this.send<T>(res, 201, data);
+	}
+
+	public clientError(res: any, message?: string): void {
+		this.send(res, 400, { message: message ?? 'Bad Request' });
+	}
+
+	public unauthorized(res: any, message?: string): void {
+		this.send(res, 401, { message: message ?? 'Unauthorized' });
+	}
+
+	public forbidden(res: any, message?: string): void {
+		this.send(res, 403, { message: message ?? 'Forbidden' });
+	}
+
+	public notFound(res: any, message?: string): void {
+		this.send(res, 404, { message: message ?? 'Not Found' });
+	}
+
+	public conflict(res: any, message?: string): void {
+		this.send(res, 409, { message: message ?? 'Conflict' });
+	}
+
+	public tooMany(res: any, message?: string): void {
+		this.send(res, 429, { message: message ?? 'Too Many Requests' });
+	}
+
+	public fail(res: any, message?: string): void {
+		this.send(res, 500, { message: message ?? 'Internal Server Error' });
+	}
 }
