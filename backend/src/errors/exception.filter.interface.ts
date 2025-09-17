ebf16a3b9ab688b57bd2bc1ac5d3eb0d112a89d5
Aziz I <zizzxiii@gmail.com>
2025-09-17 13:58:10 +0500

    feat: обновить интерфейс IExceptionFilter для поддержки обработки HttpError

--- backend/src/errors/exception.filter.interface.ts
+++ backend/src/errors/exception.filter.interface.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
+import { HttpError } from './http-error.class';
 
 export interface IExceptionFilter {
-	catch(err: Error, req: Request, res: Response, next: NextFunction): void;
+	catch(
+		err: Error | HttpError,
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): void;
 }
