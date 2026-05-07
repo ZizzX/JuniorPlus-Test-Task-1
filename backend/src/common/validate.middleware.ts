417777b16de87199cd55cd659dcbeaaf941a4a70
Aziz Isapov <zizzxiii@gmail.com>
2026-05-08 01:47:23 +0500

    feat: Add validation middleware
    Implement a middleware to validate request bodies using class-validator
    and class-transformer. This middleware will check if the request body
    conforms to the specified class structure and its validation rules.

---
+++ backend/src/common/validate.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import { IMiddleware } from './middleware.interface';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+export class ValidateMiddleware implements IMiddleware {
+	constructor(private classToValidate: ClassConstructor<object>) {}
+
+	async execute(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<void> {
+		const instance = plainToInstance(this.classToValidate, req.body);
+		const errors = await validate(instance);
+
+		if (errors.length > 0) {
+			res.status(422).send(errors);
+		} else {
+			next();
+		}
+	}
+}
