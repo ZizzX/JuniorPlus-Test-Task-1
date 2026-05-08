07c51c5aaeaf6010293537e1b06d4b48ef938482
Aziz Isapov <zizzxiii@gmail.com>
2026-05-09 00:42:27 +0500

    Remove UserLoginDto and merge into UserDto
    The UserLoginDto has been removed. Its fields have been merged into the
    existing UserDto, streamlining user data handling for both registration
    and login operations.

--- backend/src/users/dto/user-login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString } from 'class-validator';
-
-export class UserLoginDto {
-	@IsEmail({}, { message: 'Неверный формат email' })
-	email!: string;
-
-	@IsString({ message: 'Пароль обязателен' })
-	password!: string;
-}
